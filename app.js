bbfaf181f3ebcaabba05ae2131c51f8972510602
Caulonia <89051651+Caulonia@users.noreply.github.com>
2021-09-22 09:58:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.listen(process.env.PORT || 3000, ()=>{
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './register.html'));
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
 app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
 
